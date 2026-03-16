111c420da241a48f28007bb2404ccb53c36ae17d
Turki-Albogami <157608594+Turki-Albogami@users.noreply.github.com>
2026-03-16 23:03:14 +0300

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,13 @@
+-- Create Database 'DataWarehouse'
+
+USE master;
+
+CREATE DATABASE DataWarehouse;
+
+USE DataWarehouse;
+CREATE SCHEMA bronze;
+go
+CREATE SCHEMA silver;
+go
+CREATE SCHEMA gold;
+go
